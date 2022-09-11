c078cf2aeaabf92c16199d113f52f85e160c09ab
Hussains3 <shussain8978@gmail.com>
2022-09-11 14:48:25 +0600

    Try git

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The react app made by Subbir Hossin <br/>and<br/>Biswajit Sikdar</h1>
+      <h2>Designed by<br/>Ishtiuq Ahmed Chowdhury</h2>
+
+    </div>
+  );
+}
+
+export default App;
